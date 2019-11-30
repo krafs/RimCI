e40049d81ced88ded633cc862bdbbea5364df2b5
krafs <massa@krafs.net>
2019-11-30 22:29:46 +0100

    Add project files.

---
+++ RimCI/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RimCI
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            Log.Message("Test mod that was built using GitHub Actions.");
+        }
+    }
